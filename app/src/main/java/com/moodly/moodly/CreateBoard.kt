989a5b94b03f759501320dbb0f5d90aef8d8e65f
Fahad <fahadfaisalnawaz1000@gmail.com>
2025-10-31 20:15:05 +0500

    Added CreateBoard

---
+++ app/src/main/java/com/moodly/moodly/CreateBoard.kt
@@ -0,0 +1,24 @@
+package com.moodly.moodly
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class CreateBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_board)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //set current selected tab to boards in bottom nav
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.selectedItemId = R.id.nav_boards
+    }
+}
