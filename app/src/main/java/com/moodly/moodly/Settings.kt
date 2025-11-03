14a1162cae1496fba46f8bd95b8d135fc175102f
MMEHDI0606 <152722313+MMEHDI0606@users.noreply.github.com>
2025-11-03 15:35:50 +0500

    mehdi done

---
+++ app/src/main/java/com/moodly/moodly/Settings.kt
@@ -0,0 +1,32 @@
+package com.moodly.moodly
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ep)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var ep = findViewById<RelativeLayout>(R.id.edit_profile_item)
+
+        ep.setOnClickListener {
+            var intent = Intent(this, EditProfile::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
